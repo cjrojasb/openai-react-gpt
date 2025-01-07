05c72cf9c5696b8a25f7b8d8b530ff15897180ed
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2025-01-07 12:28:41 -0300

    translatePage was implemented

---
+++ src/core/use-cases/translate.use-case.ts
@@ -0,0 +1,31 @@
+import { TranslateResponse } from '../../interfaces/translate.response';
+
+export const translateUseCase = async (prompt: string, lang: string) => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_GPT_API_URL}/translate`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt, lang }),
+      }
+    );
+
+    if (!response.ok) throw new Error('Error al realizar la traducción');
+
+    const data = (await response.json()) as TranslateResponse;
+
+    return {
+      ok: true,
+      message: data.message,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'No se pudo traducir',
+    };
+  }
+};
--- src/presentation/pages/translate/TranslatePage.tsx
+++ src/presentation/pages/translate/TranslatePage.tsx
@@ -1,3 +1,81 @@
+import { useState } from 'react';
+import { GptMessage } from '../../components/chat-bubbles/GptMessage';
+import { MyMessage } from '../../components/chat-bubbles/MyMessage';
+import { TypingLoader } from '../../components/loaders/TypingLoader';
+import { TextMessageBoxSelect } from '../../components/chat-input-boxes/TextMessageBoxSelect';
+import { translateUseCase } from '../../../core/use-cases/translate.use-case';
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
+const languages = [
+  { id: 'alemán', text: 'Alemán' },
+  { id: 'árabe', text: 'Árabe' },
+  { id: 'bengalí', text: 'Bengalí' },
+  { id: 'francés', text: 'Francés' },
+  { id: 'hindi', text: 'Hindi' },
+  { id: 'inglés', text: 'Inglés' },
+  { id: 'japonés', text: 'Japonés' },
+  { id: 'mandarín', text: 'Mandarín' },
+  { id: 'portugués', text: 'Portugués' },
+  { id: 'ruso', text: 'Ruso' },
+];
+
+type Messages = Array<Message>;
+
 export const TranslatePage = () => {
-  return <div>TranslatePage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Messages>([]);
+
+  const handlePostMessage = async (message: string, selectedOption: string) => {
+    console.log({ message, selectedOption });
+    const userMessage = `Traduce: "${message}" al idioma "${selectedOption}"`;
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: userMessage, isGpt: false }]);
+    const { ok, message: text } = await translateUseCase(
+      message,
+      selectedOption
+    );
+    setIsLoading(false);
+
+    if (!ok) return alert(message);
+
+    setMessages((prev) => [
+      ...prev,
+      {
+        text,
+        isGpt: true,
+      },
+    ]);
+  };
+
+  return (
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          <GptMessage text='Qué quieres que traduzca hoy' />
+          {messages.map(({ text, isGpt }, index) =>
+            isGpt ? (
+              <GptMessage key={index} text={text} />
+            ) : (
+              <MyMessage key={index} text={text} />
+            )
+          )}
+          {isLoading && (
+            <div className='col-start-1 col-end-12 fade-in'>
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBoxSelect
+        onSendMessage={handlePostMessage}
+        placeholder='Escribe aquí un mensaje para GPT-4o'
+        disableCorrections={true}
+        options={languages}
+      />
+    </div>
+  );
 };
